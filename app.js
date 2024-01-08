0f420c41517fdaf584f9908852b85f41fbd8ec2d
SaidARSLAN <lost.myreligion@hotmail.com>
2024-01-08 22:05:50 +0300

    lesson 2

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 let http = require('http')
 
-const requestListener = (request,response) => {
+
+// Server oluşturup bir callback fonksiyon çağırdık!
+let  server = http.createServer((request,response) => {
+    // console.log(request.url,request.method)
+    // console.log(response.statusCode)
+
+    // Response başlığa sahiptir.
+    // Gönderilmiş olan bilgiler responsun body'si içerisinde gider.
+    // Ekstra bilgiler Content-Type gibi gönderilir!
+    response.setHeader("Content-Type","text/html")
+    response.statusCode = 200
+    response.statusMessage = "OK"
+    response.write("<h1>AnaSayfa</h1>")
+    response.write("<p>Urunler</p>")
+
     response.end()
-}
 
-
-let  server = http.createServer(requestListener)
+})
 
 
 server.listen(3000)
