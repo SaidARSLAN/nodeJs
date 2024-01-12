95510b15c1ca08cdf6b857fd2135dfe71b8ecf7e
SaidARSLAN <lost.myreligion@hotmail.com>
2024-01-12 14:08:01 +0300

    Mysql connection has ben established

---
+++ expressBasics/config.js
@@ -0,0 +1,10 @@
+const config = {
+    db : {
+        host : "localhost",
+        user:"root",
+        password : "Roottoor2199.",
+        database : "blogapp"
+    }
+}
+
+module.exports = config
