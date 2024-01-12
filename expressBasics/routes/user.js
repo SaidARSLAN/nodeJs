e777605d9899728f25a7549539b19a846e581f16
SaidARSLAN <lost.myreligion@hotmail.com>
2024-01-12 10:50:04 +0300

    Added condiiton and last changes

--- expressBasics/routes/user.js
+++ expressBasics/routes/user.js
@@ -3,8 +3,28 @@ const router = express.Router()
 
 const data = {
     title : "Popüler Kurslar",
-    categories : ["Web Geliştirme","Programlama","Mobil Uygulamalar","Veri Analizi","Ofis Uygulamaları"
-]
+    categories : ["Web Geliştirme","Programlama","Mobil Uygulamalar","Veri Analizi","Ofis Uygulamaları"],
+    blogs : [
+        {
+            blogid : 1,
+            baslik : "Komple Web Geliştirme",
+            aciklama : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis quae sed debitis praesentium molestiae, laboriosam iste aliquid quam repellat dolore reiciendis necessitatibus corporis repellendus iusto soluta in amet natus? Reiciendis?",
+            resim : "1.jpeg",
+
+        },
+        {
+            blogid : 2,
+            baslik : "Python Programlama",
+            aciklama : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis quae sed debitis praesentium molestiae, laboriosam iste aliquid quam repellat dolore reiciendis necessitatibus corporis repellendus iusto soluta in amet natus? Reiciendis?",
+            resim : "2.jpeg",
+        },
+        {
+            blogid : 3,
+            baslik : "Veri Analizi Eğitimi",
+            aciklama : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatis quae sed debitis praesentium molestiae, laboriosam iste aliquid quam repellat dolore reiciendis necessitatibus corporis repellendus iusto soluta in amet natus? Reiciendis?",
+            resim : "3.jpeg",
+        }
+    ]
 }
 
 router.use("/blogs/:id",(request,response) => {
