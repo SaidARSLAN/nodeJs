858dd456db76f54945cbb2aa6ce0bf0addf46ec5
SaidARSLAN <lost.myreligion@hotmail.com>
2024-01-13 16:50:59 +0300

    Kullanıcının ekleme yapacağı form eklendi. Database işlemlerine geçilecek!

--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-router.use("/blog/create", function(req, res) {
-    res.render("admin/blog-create");
+const db = require('../data/db')
+
+
+
+router.use("/blogs/create", async function(req, res) {
+    try {
+
+        // Birincisi sorgudan dönecekler bilgiler, ikincisi kolon bilgileri
+        const [categories, ] = await db.execute("select * from category");
+
+        res.render("admin/blog-create", {
+            title : "add blog",
+            categories : categories
+        });
+    }
+    catch (err) {
+        console.log(err)
+    }
+    
 });
 
 router.use("/blogs/:blogid", function(req, res) {
